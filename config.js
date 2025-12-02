1bd180985b3857964cd7b926a770bb7b07231376
DominicDev <s0l0estudi4r@gmail.com>
2025-12-02 12:36:24 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LÍUE';
+export const nameSongs = [
+	"SUFFER WITH ME (SLOWED DOWN)", /*1*/
+	"PHANTOM BRUISE", /*2*/
+	"LIQUID ₱AIN", /*3*/
+	"IN THE DARK", /*4*/
+	"SECRET PRISONER", /*5*/
+	"BLUE", /*6*/
+	"SPIRAL TREE", /*7*/
+	"RAKION", /*8*/
+	"SOUL", /*9*/
+	"SEXION", /*10*/
+	"VOID", /*11*/
+	"ALPHA", /*12*/
+	"ABYSMAL", /*13*/
+	"DELEGATE MISERY", /*14*/
+	"DESIGNED 2 DIE YOUNG", /*15*/
+	"FIELD SPELL", /*16*/
+	"AGONY EYES", /*17*/
+	"MAKE ME FORGET", /*18*/
+	"SECRET PRISONER", /*19*/
+	"SOAS (OUTRO)", /*20*/
+]
